68af2195b367cb1e49f7b1d1991705b2cc79cc46
xHyroM <generalkubo@gmail.com>
2022-07-26 22:27:19 +0200

    fix: update summary

--- 3rd-packages/nanoid/summary.mjs
+++ 3rd-packages/nanoid/summary.mjs
@@ -1,13 +1,15 @@
 import summary from "../../scripts/summary.mjs";
 import { join } from "node:path";
 
-const outputBun = JSON.parse(await (await Bun.file(join(__dirname, "outputs", "bun.json"))).text()).benchmarks.map(b => {
+console.log(await Bun.file(join(__dirname, "outputs", "bun.json")).text());
+
+const outputBun = JSON.parse(await Bun.file(join(__dirname, "outputs", "bun.json")).text()).benchmarks.map(b => {
     return { ...b, name: `[bun] ${b.name}`, id: b.name };
 });
-const outputNode = JSON.parse(await (await Bun.file(join(__dirname, "outputs", "node.json"))).text()).benchmarks.map(b => {
+const outputNode = JSON.parse(await Bun.file(join(__dirname, "outputs", "node.json")).text()).benchmarks.map(b => {
     return { ...b, name: `[node] ${b.name}`, id: b.name };
 });
-const outputDeno = JSON.parse(await (await Bun.file(join(__dirname, "outputs", "deno.json"))).text()).benchmarks.map(b => {
+const outputDeno = JSON.parse(await Bun.file(join(__dirname, "outputs", "deno.json")).text()).benchmarks.map(b => {
     return { ...b, name: `[deno] ${b.name}`, id: b.name };
 });
 
