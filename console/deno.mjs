dc5b1ad5e0fa2508aa59230c0bd65021a3547532
Hyro <generalkubo@gmail.com>
2022-07-26 23:28:25 +0200

    benchmarks(console): update

--- console/deno.mjs
+++ console/deno.mjs
@@ -5,8 +5,5 @@ const __dirname = new URL('.', import.meta.url).pathname;
 
 bench("console.log('hello')", () => console.log("hello"));
 bench("console.log({ hello: 'object' })", () => console.log({ hello: "object" }));
-bench("console.info('hello')", () => console.info("hello"));
-bench("console.warn('hello')", () => console.warn("hello"));
-bench("console.debug('hello')", () => console.debug("hello"));
 
 await save(await run(), "deno", __dirname);
