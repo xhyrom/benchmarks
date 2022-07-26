b9dca65024a6f75a92f0439b97cdde6491b1f34e
xHyroM <generalkubo@gmail.com>
2022-07-26 22:28:54 +0200

    fix: update summary

--- console/summary.mjs
+++ console/summary.mjs
@@ -1,6 +1,8 @@
 import summary from "../scripts/summary.mjs";
 import { join } from "node:path";
 
+console.log(await Bun.file(join(__dirname, "outputs", "bun.json")).text());
+
 const outputBun = JSON.parse(await Bun.file(join(__dirname, "outputs", "bun.json")).text()).benchmarks.map(b => {
     return { ...b, name: `[bun] ${b.name}`, id: b.name };
 });
