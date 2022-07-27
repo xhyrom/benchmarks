67fbf0b1350a322596e6bf53ea3ef7b1c8368275
xHyroM <generalkubo@gmail.com>
2022-07-27 18:13:51 +0200

    benchmarks(buffer): add decode group

--- benchmarks/buffer/node.mjs
+++ benchmarks/buffer/node.mjs
@@ -15,7 +15,19 @@ group("encode", () => {
     bench("Buffer.from('hello').toString('hex')", () => Buffer.from("hello").toString("hex"));
 });
 
+group("decode", () => {
+    bench("Buffer.from('hello', 'ascii').toString()", () => Buffer.from("hello", "ascii").toString());
+    bench("Buffer.from('hello', 'utf8').toString()", () => Buffer.from("hello", "utf-8").toString());
+    bench("Buffer.from('敨汬', 'utf16le').toString()", () => Buffer.from("敨汬", "utf16le").toString());
+    bench("Buffer.from('敨汬', 'ucs2').toString()", () => Buffer.from("敨汬", "ucs2").toString());
+    bench("Buffer.from('aGVsbG8=', 'base64').toString()", () => Buffer.from("aGVsbG8=", "base64").toString());
+    bench("Buffer.from('aGVsbG8', 'base64url').toString()", () => Buffer.from("aGVsbG8", "base64url").toString());
+    bench("Buffer.from('hello', 'latin1').toString()", () => Buffer.from("hello", "latin1").toString());
+    bench("Buffer.from('hello', 'binary').toString()", () => Buffer.from("hello", "binary").toString());
+    bench("Buffer.from('68656c6c6f', 'hex').toString()", () => Buffer.from("68656c6c6f", "hex").toString());
+});
+
 await save(await run(), "node", __dirname, {
     encode: (name) => name.match(/\.toString\('[aA-zZ0-9]+'\)/)[0],
-    decode: (name) => name.match(/\.from\('[aA-zZ0-9]+'\, '[aA-zZ0-9]+'\)/)[0]
+    decode: (name) => name.match(/\.from\('(.*?)+'\, '[aA-zZ0-9]+'\)/)[0]
 });
