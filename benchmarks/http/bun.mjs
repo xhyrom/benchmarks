f224014e69d156ade211555320020a32f5cd48c9
Hyro <generalkubo@gmail.com>
2022-07-28 22:43:20 +0200

    fix(http/bun): correct pid

--- benchmarks/http/bun.mjs
+++ benchmarks/http/bun.mjs
@@ -5,7 +5,7 @@ import { exec, execAndDontWait } from "bun-utilities";
 execAndDontWait(["bun", "servers/bun.mjs"]);
 const rawPid = await exec(["bash", "-c", "ps | grep bun"]);
 const splittedPid = rawPid.stdout.split('\n');
-const pid = splittedPid[splittedPid.length - 2].split(' ')[0];
+const pid = splittedPid[splittedPid.length - 2].replaceAll(' ', '').replaceAll('00:00:00', '').replaceAll('bun', '');
 
 await save(await run(), "bun", __dirname);
 console.log(pid, splittedPid);
