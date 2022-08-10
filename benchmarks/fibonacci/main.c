7fee1668d554d44d0ffaaea3a78a6b8ef17c2da6
xHyroM <generalkubo@gmail.com>
2022-08-10 22:04:36 +0200

    benchmarks(fibonacci): add, add c lang

---
+++ benchmarks/fibonacci/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint64_t fibo(register uint64_t n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main() {
+    fibo(50);
+    return 0;
+}
---
+++ benchmarks/fibonacci/main.cr
@@ -0,0 +1,9 @@
+def fibo(n : UInt64) : UInt64
+    if n <= 1
+        return n
+    end
+    
+    return fibo(n - 1) + fibo(n - 2)
+end
+
+fibo(50);
